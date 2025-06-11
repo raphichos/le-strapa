32293cc161b5d2bc21fc908fa5f70a36f4d0f5e2
raphichos <rios.raph@gmail.com>
2025-06-10 22:22:44 -0300

    fix

--- sw.js
+++ sw.js
@@ -1,21 +1,20 @@
-
-self.addEventListener('install', event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open('le-strapa-cache').then(cache => {
+    caches.open("le-strapa-cache").then((cache) => {
       return cache.addAll([
-        './',
-        './index.html',
-        './audio/amore_mio_aiutami.mp3',
-        './manifest.json',
-        './icons/icon-192.png'
+        "./",
+        "./index.html",
+        "./audio/amore_mio_aiutami.mp3",
+        "./manifest.json",
+        "./icon/icon-192.png",
       ]);
     })
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => {
+    caches.match(event.request).then((response) => {
       return response || fetch(event.request);
     })
   );
