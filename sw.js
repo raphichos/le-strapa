71fcf9944b5b4eade2d8ae3badcb0c5c837dd3db
raphichos <rios.raph@gmail.com>
2025-06-10 22:17:21 -0300

    initial commit

---
+++ sw.js
@@ -0,0 +1,22 @@
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('le-strapa-cache').then(cache => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './audio/amore_mio_aiutami.mp3',
+        './manifest.json',
+        './icons/icon-192.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
